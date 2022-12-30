5380a44d8b4d4e8c78c02c33ad637bb432a1dafb
Ubaldo Lopez <ulopez293@gmail.com>
2022-12-30 16:21:31 -0600

    servicios

---
+++ js/servicios.js
@@ -0,0 +1,190 @@
+
+const TEXTOS = [
+    {
+        titulo: 'Comercial / Mercantil',
+        texto: `
+        El Derecho Mercantil es el Derecho Privado especial que tiene por objeto al empresario, al estatuto jurídico de ese empresario y a la peculiar actividad que éste desarrolla en el mercado. Regula los actos de comercio con independencia de que se realicen o no por comerciantes y de que los sujetos sean o no comerciales en sentido económico.
+        Los tratados clásicos de Derecho Mercantil conciben este Derecho como un Derecho Privado especialmente destinado a los comerciantes. Originariamente el Derecho Mercantil fue un Derecho de los comerciantes en el sentido de que sólo a ellos se aplicaba y un Derecho de actos de comercio porque los actos de los comerciantes ajenos a su actividad comercial quedaban al margen de esta regulación.
+
+El Derecho Mercantil surge en la Edad Media, en los siglos XI y XII, como respuesta a la necesidad de regular la denominada economía urbana y comercial frente a la precedente de carácter feudal y agraria. Ahora bien, en la evolución histórica el Derecho Mercantil ha conducido a la aplicación del Derecho Mercantil a las operaciones que el legislador ha definido como actos de comercio con independencia de que se realicen o no por comerciantes y de que los actos sujetos sean o no comerciales en sentido económico.
+
+Es aquel que regula los actos de comercio pertenecientes a la explotación de industrias mercantiles organizadas (actos de comercio propios) y los realizados ocasionalmente por comerciantes y no comerciantes (actos de comercio impropios), que el legislador considera mercantiles, así como el estatuto del comerciante o empresario mercantil individual y social y los estados de anormalidad en el cumplimiento de sus obligaciones.
+
+En la actualidad también se incluyen dentro del Derecho Mercantil instituciones del denominado Derecho de la competencia. Se trata de instituciones que han surgido de manera autónoma pero que se han ido integrando por la doctrina y la evolución legislativa en la denominada propiedad industrial (marcas, nombres comerciales, patentes, modelos de utilidad, diseño industrial) y en el Derecho de la competencia (competencia desleal y defensa de la competencia).
+        `,
+    },
+    {
+        titulo: 'Derecho Laboral',
+        texto: `
+        El derecho laboral o derecho del trabajo es el conjunto de normas que regulan la relación entre los trabajadores y el patrón o empresario. Este conjunto de leyes abordan aspectos fundamentales de la relación laboral. Por ejemplo: el contrato de trabajo, los límites de la jornada laboral, la protección de la salud, el salario, los días de descanso, las vacaciones o el despido.
+
+Hoy en día, es habitual dar por sentados ciertos aspectos de la relación laboral entre los trabajadores y sus empleadores, como que el número de horas máximo que según la ley puede laborar una persona en México se de 48 horas. Sin embargo, los derechos y obligaciones laborales no han existido siempre.
+
+La Revolución Industrial que arrancó a finales del siglo XVIII en Inglaterra y se fue extendiendo por toda Europa supuso un punto de inflexión en la forma en que se producían los bienes y en las relaciones entre empresarios y trabajadores. Las tensiones derivadas de la industrialización por las duras condiciones de las fábricas y los abusos de los empresarios cristalizaron, en distintos momentos, en la regulación jurídica de esta relación.
+
+Desde mediados del siglo XIX, el derecho laboral comienza a desarrollarse de manera específica en distintos ordenamientos. El 28 de agosto de 1931 se promulgó en México la primera Ley Federal del Trabajo.
+        `,
+    },
+    {
+        titulo: 'Derecho Penal',
+        texto: `
+        El gobierno usa el derecho penal, como parte del sistema judicial, para procesar a una persona acusada de un acto ilegal. La persona acusada, juzgada y condenada en un caso criminal puede ser encarcelada, multada, o ambos.
+
+Los casos penales son fundamentalmente distintos a los civiles. El proceso penal tiene el propósito de castigar al que comete un crimen. Mientras tanto, un proceso civil intenta proteger los derechos privados, corregir daños en un asunto privado o poner vigentes las obligaciones privadas. En un caso penal, un procurador o fiscal del estado trata de comprobar los cargos contra el acusado. Pero, los casos civiles involucran conflictos entre individuos, empresas privadas o una entidad del gobierno.
+
+Los actos criminales son clasificados como felonías o delitos menores. Las felonías usualmente son castigadas con sentencias de un año de prisión o más. Los delitos menores generalmente se castigan con menos de un año. Sin embargo, ningún acto es considerado crimen si no lo declara el código penal o el derecho común.
+
+Esta sección de FuenteLegal, le provee información sobre la defensa penal e incluye artículos sobre las infracciones de transito, el manejo en estado de ebriedad (DWI o DUI) y muchos temas mas.
+        `,
+    },
+    {
+        titulo: 'Derecho Civil',
+        texto: `
+        Derecho Civil ha de entenderse el conjunto de normas jurídicas, que conformando el Derecho privado general, regulan todos los aspectos atinentes a la personalidad, la familia y las relaciones patrimoniales.
+        El Derecho Civil, tal y como lo entendemos en la actualidad, proviene del Derecho romano, el cual regulaba todo lo concerniente al ciudadano romano, y por tanto, contenía tanto normas de Derecho Privado como normas de Derecho Público. El Derecho romano seguía el principio de la personalidad del Derecho, esto es, cada pueblo vivía según sus propias normas, su propio y exclusivo Derecho. Posteriormente, y cuando Roma entra en contacto con otros pueblos, es cuando surge la necesidad de diferenciar entre las normas aplicables a los ciudadanos romanos y las normas que regulaban las relaciones de Roma con el resto de pueblos extranjeros. Surge, de este modo, la diferenciación entre Derecho Público y Derecho Privado.
+        `,
+    },
+    {
+        titulo: 'Derecho Sanitario',
+        texto: `
+        El Derecho sanitario confirma a la salud como derecho fundamental (proclamado por la ONU), y además regula el Sistema Nacional de Salud, cuyos actores principales son: las autoridades sanitarias nacionales que ejercen la rectoría del mismo, las autoridades sanitarias locales que promueven sus mandatos 
+        `,
+    },
+    {
+        titulo: 'Derecho Ambiental y Urbano',
+        texto: `
+        El derecho ambiental o derecho de la naturaleza es la rama del derecho público que estudia las relaciones entre los seres humanos y la naturaleza, con el fin de crear y comprender las normas jurídicas que preservan y protegen el medio ambiente de las actividades perjudiciales consecuencia de la actividad industrial y humana.
+
+Dicho en términos más simples, se trata de una rama del derecho que se encarga de la regulación ecológica de las actividades humanas, con el cometido de promover un modelo de civilización que no amenace la continuidad de la existencia de la vida en el planeta, ni tampoco la empobrezca de manera drástica e irreversible.
+
+Es una rama del derecho sumamente reciente, que posee un notorio carácter multidisciplinar, ya que involucra la experticia de profesionales de muchos campos distintos, como la biología, la medicina, la ecología, la física, la química, etc. Requiere de una actualización constante de sus perspectivas, debido a los veloces cambios científico-tecnológicos actuales.
+
+El derecho ambiental surgió como disciplina independiente en 1972, en la Conferencia de Naciones Unidas sobre el Medio Humano, celebrada en Estocolmo.
+
+Fuente: https://concepto.de/derecho-ambiental/#ixzz7ozaOuo7t
+
+        `,
+    },
+    {
+        titulo: 'Derecho Administrativo',
+        texto: `
+        En sentido amplio el derecho administrativo corresponde al conjunto de las normas del derecho privado y del derecho público que se aplican a la administración en su gestión de los servicios públicos y en sus relaciones con los particulares. En un sentido más restringido, pero admitido comúnmente, el derecho administrativo se extiende exclusivamente a las normas que derogan el derecho privado y que son por tanto normalmente aplicadas por las jurisdicciones administrativas.
+
+        Es el conjunto de normas legales, doctrina y técnicas que regulan la actividad de la Administración Pública en cuanto se relaciona con los administrados o en cuanto unos órganos de aquélla se relacionan entre sí con motivo de la función administrativa, primando siempre el interés público sobre el particular. No forma parte de éste el estudio de las figuras jurídicas de derecho privado que, en frecuentes ocasiones, utiliza la Administración Pública para satisfacer determinados intereses propios de un organismo o entidad administrativa. De ahí que no pueda afirmarse que Derecho administrativo es aquél que regula la actividad administrativa. La esencia diferencial del Derecho administrativo radica en la nota de preeminencia que favorece a la Administración Pública situándola en un nivel superior al administrado en cuanto aquélla realiza actividades de interés general. En definitiva, el Derecho administrativo es el Derecho público común y general.
+        
+        El concepto aludido ha sido criticado por su vaguedad y amplitud. Por tal motivo y sin pretender que sea una noción inobjetable, creemos que podría definirse este sector jurídico diciendo que es el que rige la organización y funcionamiento de los servicios públicos, de los demás servicios administrativos y la restante actividad de la Administración pública, así como las relaciones entre esta última y los particulares, por razón de dichas actividades.
+        `,
+    },
+    {
+        titulo: 'Representación ante aseguradoras',
+        texto: `
+        
+
+El perjudicado podrá presentar la reclamación ante la entidad aseguradora establecida en Mexico o ante el representante designado por ésta .
+
+La Entidad aseguradora o su representante darán contestación a la reclamación en un plazo de tres meses desde su presentación, debiendo presentarse oferta motivada si se ha determinado la responsabilidad y cuantificado el daño. En caso contrario, o si la reclamación hubiera sido rechazada, dará respuesta motivada a lo planteado en la reclamación.
+
+Transcurrido el plazo mencionado en el apartado anterior sin que se haya presentado una oferta motivada, se devengarán intereses de demora de acuerdo con lo previsto en la legislación que en cada caso resulte de aplicación, en atención al lugar de ocurrencia del siniestro.
+
+La no contestación al perjudicado por la entidad aseguradora o su representante (sea por admisión de la reclamación o por su rechazo motivado) constituye infracción administrativa grave o leve de acuerdo con lo dispuesto en los artículos 40.4.t y 40.5.d de la Ley de ordenación y supervisión de los seguros privados (texto refundido aprobado por el R.D.L. 6/2004, de 29 de octubre).
+
+La acción del representante para la tramitación y liquidación de siniestros no será suficiente para modificar el derecho material que se haya de aplicar en el caso concreto, ni para atribuir la competencia a los órganos jurisdiccionales del Estado miembro de residencia del perjudicado, salvo lo previsto en las normas de derecho internacional público y privado sobre la Ley aplicable a los accidentes de circulación y sobre la atribución de competencias jurisdiccionales.
+
+        `,
+    },
+    {
+        titulo: 'Propiedad Intelectual',
+        texto: `
+        El Derecho de Propiedad Intelectual es una rama del Derecho Público y tiene por objeto la protección de las creaciones de la mente, tales como invenciones, obras literarias y artísticas, símbolos, nombres e imágenes que se utilizan en el comercio.
+
+        Actualmente en nuestro país el Estado muestra un mayor interés en impulsar y promover la actividad inventiva, dando una mayor difusión a los derechos de propiedad intelectual, a los beneficios y seguridad jurídica que la propiedad intelectual otorga a los diversos sectores productivos y a la competencia económica, así como en la satisfacción de los intereses y necesidades de los consumidores.
+        
+        La propiedad intelectual es protegida a través de la propiedad industrial y los derechos de autor. 
+        `,
+    },
+    {
+        titulo: 'Internacional / Migratorio',
+        texto: `
+        El Derecho Internacional es la rama jurídica que regula las relaciones entre Estados y particulares, siempre que tengan un componente internacional, con el objetivo de evitar conflictos. Se puede aplicar a dos niveles: en las relaciones que se producen entre dos o más países y en las relaciones jurídicas de ciudadanos que tengan carácter internacional.
+
+El Derecho Internacional surge de la necesidad de establecer un marco que regulase conflictos frecuentes entre países. Por ejemplo, los derechos para explotar aguas que podían ser dominio de varios territorios o el establecimiento de fronteras. Los antecedentes del Derecho Internacional se remontan a tratados de paz como el de Westfalia de 1648, que puso fin a la Guerra de los 30 años en Alemania y a la de los 80 años entre España y Países Bajos. Más adelante, declaraciones como la de París de 1853 sobre la guerra en el mar o los Convenios de Ginebra sobre el derecho internacional en cuestiones humanitarias, han sido muy relevantes para sentar las bases de este corpus normativo internacional.
+        `,
+    },
+    {
+        titulo: 'Seguros y Finanzas',
+        texto: `
+        Los seguros y las fianzas son los instrumentos que le garantizan a los usuarios y a las empresas tener una protección total o parcial en caso de existir riesgos personales, empresariales y de propiedades.
+
+En México, el sector asegurador a crecido exponencialmente, debido a la necesidad que tienen los usuarios de resguardar sus vidas, su salud y la de familiares, así como también la de sus propiedades. También existen leyes que establecen que los usuarios deben asegurar alguna de sus propiedades contra daños, robos o pérdidas (este es el caso de los automóviles), o adquirir un seguro de responsabilidad civil para los dueños de negocios.
+
+Si deseas conocer más acerca de los seguros y las fianzas, continúa leyendo el artículo.
+        `,
+    },
+    {
+        titulo: 'Fiscal',
+        texto: `
+        El Derecho Fiscal lo definimos como la rama del Derecho Público que comprende el conjunto de normas jurídicas que regulan las relaciones jurídicas entre el Estado, en su carácter de autoridad fiscal y los gobernados en su carácter de contribuyentes.
+
+El Derecho Fiscal establece las normas que determinan los tributos. Esto es, a través de los diversos ordenamientos fiscales el Estado grava las actividades de los gobernados sin importar la naturaleza civil o mercantil de las mismas.
+        `,
+    },
+    {
+        titulo: 'Telecomunicaciones',
+        texto: `
+El Derecho de Telecomunicaciones está constituido básicamente por
+disposiciones constitucionales, Leyes, Reglamentos, Decretos, Ordenanzas, etc.,
+en materia de telecomunicaciones que conforman esta rama. Durante el
+desarrollo del presente trabajo investigativo se abordaron temas importantes en la
+historia de las telecomunicaciones del Ecuador y lo aspectos jurídicos
+relacionados con el tema, por ejemplo hemos visto la creación de los órganos que
+se han establecido como actores del sector, así por ejemplo tenemos al Consejo
+Nacional de Telecomunicaciones (CONATEL) quien es el ente encargado de la
+administración y regulación de las telecomunicaciones en el Ecuador, la
+Secretaria Nacional de Telecomunicaciones (SENATEL) quien es el representante
+del Estado ante la UIT y la Superintendencia de Telecomunicaciones
+(SUPERTEL) quien se encarga del control y monitoreo del espectro radioeléctrico.
+        `,
+    },
+    {
+        titulo: 'Energia',
+        texto: `
+        En cuanto a la Reforma Energética, entendamos que si bien los responsables de su formulación tuvieron como ejemplo grandes fracasos y éxitos de estados y países que abrieron el mercado de los hidrocarburos, lo cierto es que entraron a un terreno desconocido. El derecho energético debió trabajar de cerca con temas técnicos para su debida implementación.
+
+El plan para la Reforma Energética comenzó como un paso decidido hacia la modernización del sector energético, en el que para lograr un debido desarrollo y para que se llevaran a cabo las mejores prácticas, era necesario crear y seguir construyendo un marco jurídico que se encontrara sustentado en el mercado, lo cual debe tener como objetivo final el exponencializar el desarrollo de nuestro país. 
+
+El derecho energético, por su parte, permite que el sector privado tenga seguridad jurídica al dedicarse e involucrarse en la exploración, extracción, distribución, transporte, comercialización, así como demás actividades del sector energético, ya sea de hidrocarburos o de electricidad, lo que se traduce en estabilidad para el proyecto a realizar e incluso atrae inversión extranjera. Esto no solo fortalece el sector privado, sino también a las Empresas Productivas del Estado para convertirlos en entes realmente competitivos. 
+
+Por otro lado, el derecho energético, también juega un papel importante en el cambio climático, pues es una realidad que la producción de energía basada en combustibles fósiles debe desaparecer, pues a la fecha siguen siendo una fuente de energía importante que es utilizada para el desarrollo de nuestro país, por lo que es necesario contemplar una transición para poder pasar a un desarrollo sostenible. 
+
+Dadas las implicaciones económicas que representan los proyectos del sector energético, es que el derecho se vuelve sumamente importante para el correcto desarrollo y ejecución de los mismos, lo cual se traduce en no solo seguridad energética para el país, sino también para su desarrollo en general. 
+        `,
+    },
+    {
+        titulo: 'Inmobiliario',
+        texto: `
+        El derecho inmobiliario es el área del derecho encargada de regir la compra, venta y uso de terrenos o cualquier tipo de propiedad. 
+
+Se trata de un conjunto de normas y leyes que rigen el modo en que las personas pueden y deben adquirir propiedad y lo que pueden hacer con aquellas que les pertenecen.  
+
+Se rige bajo la ley de propiedad, que es a la que competen las transacciones relacionadas con cualquier propiedad comercial o residencial y que cubre áreas especializadas, como financiación, préstamos hipotecarios, entre otros. 
+        `,
+    },
+    {
+        titulo: 'Compliance',
+        texto: `
+        El compliance es un programa de cumplimiento incorporado por muchas empresas para detectar y prevenir posibles riesgos legales relacionados con la compañía.
+        A través de este método se trata de evitar posibles situaciones desfavorables para la empresa, desde la comisión de delitos o conductas abusivas (compliance penal) hasta el impacto negativo sobre el medio ambiente, y ajustar todos sus actos al marco regulatorio vigente.
+        `,
+    },
+]
+
+function desplegarModal(e) {
+    if (e.target.nodeName != 'I') return
+    let idPosition = parseInt(e.target.getAttribute('id')) - 1
+    let myModal = new bootstrap.Modal(document.getElementById("myModal"), {})
+    document.getElementById('myModalTitle').textContent = TEXTOS[idPosition].titulo
+    document.getElementById('myModalBody').textContent = TEXTOS[idPosition].texto
+    myModal.show()
+}
+
+document.getElementById('contenidoPracticas').addEventListener('click', desplegarModal)
